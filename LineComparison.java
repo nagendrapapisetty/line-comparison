50ba69fb8070c3ab41a2cc984802d253180dacef
unknown <papisettynagendra>
2022-08-14 16:59:20 +0530

    added:claculate lenth of line

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,25 @@
 package com.girdgelabz.linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
-    public static void main(String[] args) {
-        System.out.println("Welcome Line Comparison");
+        public static void Line(int x1, int x2, int y1, int y2) {
+            double line;
+            line = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            System.out.println("distancebetween" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") is " + line);
+        }
+
+        public static void main(String[] args) {
+            int x1, x2, y1, y2;
+            System.out.println("Enter x1,y1 and x2,y2");
+            Scanner sc = new Scanner(System.in);
+            x1 = sc.nextInt();
+            y1 = sc.nextInt();
+            x2 = sc.nextInt();
+            y2 = sc.nextInt();
+            Line(x1, x2, y1, y2);
+
+
+        }
+
     }
-}
